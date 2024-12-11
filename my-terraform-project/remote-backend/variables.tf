a2be933b74476bbbdf9dc1b1afb1985b3af0606d
mariemkahli1 <kahli.mariem@enis.tn>
2024-12-11 14:05:23 +0100

    Update variables.tf

--- my-terraform-project/remote-backend/variables.tf
+++ my-terraform-project/remote-backend/variables.tf
@@ -1,13 +1,13 @@
-# backend/variables.tf
 # S3 Bucket Name
 variable "s3_bucket_name" {
   description = "Name of the S3 bucket to store Terraform state"
   type        = string
-  default     = "enis-terraform-for-state-file"
+  default     = "enis-terraform-for-state-file-0125"
 }
+
 # DynamoDB Table Name
 variable "dynamodb_table_name" {
   description = "Name of the DynamoDB table for state locking"
   type        = string
-  default     = "terraform-state-locking"
+  default     = "terraform-state-locking-1256"
 }
