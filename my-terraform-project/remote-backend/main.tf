65dd4521be61c336cd568d91d13f741bfe2af215
mariemkahli1 <kahli.mariem@enis.tn>
2024-12-11 15:54:15 +0100

    Update main.tf

--- my-terraform-project/remote-backend/main.tf
+++ my-terraform-project/remote-backend/main.tf
@@ -1,5 +1,11 @@
 terraform {
-  required_providers {
+  backend "s3" {
+    bucket         = "yes-4fc573f3"
+    key            = "terraform/state"
+    region         = "us-east-1"
+    dynamodb_table = "yes"
+  }
+required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 3.0"
