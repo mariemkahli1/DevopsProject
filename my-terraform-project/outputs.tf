69d87c814d8f5c8f62e896bb3b5d6de0bdd766f2
mariemkahli1 <kahli.mariem@enis.tn>
2024-12-11 14:09:08 +0100

    Update outputs.tf

--- my-terraform-project/outputs.tf
+++ my-terraform-project/outputs.tf
@@ -1,19 +1,20 @@
 output "instance_details" {
   description = "Detailed information about the EC2 instance"
   value = {
-    instance_id          = aws_instance.public_instance.id
-    instance_public_ip   = aws_instance.public_instance.public_ip
-    instance_private_ip  = aws_instance.public_instance.private_ip
-    instance_name        = aws_instance.public_instance.tags["Name"]
-    security_group_ids   = aws_instance.public_instance.vpc_security_group_ids  # All security groups attached to the instance
-    subnet_id            = aws_instance.public_instance.subnet_id
+    instance_id         = aws_instance.public_instance.id
+    instance_public_ip  = aws_instance.public_instance.public_ip
+    instance_private_ip = aws_instance.public_instance.private_ip
+    instance_name       = aws_instance.public_instance.tags["Name"]
+    security_group_id   = aws_instance.public_instance.vpc_security_group_ids
+    subnet_id           = aws_instance.public_instance.subnet_id
   }
 }
+# Output the S3 URI of the stored private key
 output "deployer_key_s3_uri" {
   description = "S3 URI of the deployer key file"
-  value       = "s3://${aws_s3_object.private_key_object.bucket}/${aws_s3_object.private_key_object.key}"
+  value       = "s3://${aws_s3_bucket_object.private_key_object.bucket}/${aws_s3_bucket_object.private_key_object.key}"
 }
-
+# Output the RDS endpoint (hostname)
 output "rds_endpoint" {
   description = "The endpoint of the RDS database"
   value = {
