ba67c9bdb35b7c1458736cdf3bc18472cba81b66
mariemkahli1 <kahli.mariem@enis.tn>
2024-12-11 14:04:31 +0100

    Update main.tf

--- my-terraform-project/remote-backend/main.tf
+++ my-terraform-project/remote-backend/main.tf
@@ -1,4 +1,3 @@
-# backend/main.tf
 terraform {
   required_providers {
     aws = {
@@ -7,42 +6,44 @@ terraform {
     }
     random = {
       source  = "hashicorp/random"
-      version = "~> 3.0"  # Specify a version constraint
-    }
-    tls = {
-      source  = "hashicorp/tls"
       version = "~> 3.0"
     }
   }
+
   backend "s3" {
-    bucket= "enis-terraform-for-state-file-0125-6735d762"  # Replace with your S3 bucket name
-    key = "aws-backend/terraform.tfstate" # Location of the state file in the bucket
-    region = "us-east-1" # AWS region
-    dynamodb_table = "terraform-state-locking-1256" # Replace with your DynamoDB table name
-    encrypt = true  # Enables encryption for the state file
+    bucket         = "custom-terraform-state-bucket-123456-0f4b4cef" # Replace with your S3 bucket name
+    key            = "aws-backend/terraform.tfstate" # Location of the state file in the bucket
+    region         = "us-east-1" # AWS region
+    dynamodb_table = "custom-terraform-state-locks-123456" # Updated DynamoDB table name
+    encrypt        = true # Enables encryption for the state file
   }
 }
+
+# AWS provider configuration
 provider "aws" {
   region = "us-east-1"
 }
 
+# Generate a random ID for bucket uniqueness
 resource "random_id" "bucket_suffix" {
   byte_length = 4
 }
 
-# S3 Bucket for Terraform State
+# S3 bucket for storing Terraform state
 resource "aws_s3_bucket" "terraform_state" {
   bucket        = "${var.s3_bucket_name}-${random_id.bucket_suffix.hex}"
-  force_destroy = false  # Ensures the bucket is not destroyed when running 'terraform destroy'
+  force_destroy = true
 }
 
-
+# Enable versioning for the S3 bucket
 resource "aws_s3_bucket_versioning" "terraform_bucket_versioning" {
   bucket = aws_s3_bucket.terraform_state.id
   versioning_configuration {
     status = "Enabled"
   }
 }
+
+# Enable server-side encryption for the S3 bucket
 resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state_crypto_conf" {
   bucket = aws_s3_bucket.terraform_state.bucket
   rule {
@@ -51,10 +52,13 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state_c
     }
   }
 }
+
+# DynamoDB table for state locking
 resource "aws_dynamodb_table" "terraform_locks" {
-  name         = var.dynamodb_table_name
+  name         = "custom-terraform-state-locks-123456" # Match the table name with the existing table
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
+
   attribute {
     name = "LockID"
     type = "S"
